f74acfb24a7ad99e5d079e1682be66912282d400
Garett Dunn <garettmd@gmail.com>
2015-01-26 17:00:55 -0600

    Split out file into challenge-specific ones

---
+++ elevator6.js
@@ -0,0 +1,25 @@
+{
+    init: function(elevators, floors) {
+        var elevator1 = elevators[0];
+        elevator1.on("idle", function() {
+            elevator1.goToFloor(0);
+        });
+        elevator1.on("floor_button_pressed", function(floorNum){
+            elevator1.goToFloor(floorNum);
+        });
+        
+        var elevator2 = elevators[1];
+        elevator2.on("idle", function() {
+            elevator2.goToFloor(0);
+        });
+        elevator2.on("floor_button_pressed", function(floorNum){
+            elevator2.goToFloor(floorNum);
+        });
+		
+		var floor1 = floors[1];
+		floor1.on(
+    },
+    update: function(dt, elevators, floors) {
+
+    }
+}
